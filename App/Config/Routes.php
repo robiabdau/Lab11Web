57f8b3a4aec67dababc4bceaac46b1079465563b
Robi Abda'u <81896427+robiabdau@users.noreply.github.com>
2021-06-23 21:51:36 +0700

    Add files via upload

--- App/Config/Routes.php
+++ App/Config/Routes.php
@@ -33,12 +33,17 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->get('/home', 'Page::home');
-$routes->get('/artikel', 'Page::artikel');
 $routes->get('/about', 'Page::about');
 $routes->get('/contact', 'Page::contact');
 $routes->get('/faqs', 'Page::faqs');
-
+$routes->get('/artikel/(:any)', 'Artikel::view/$1');
+$routes->group('admin', function($routes) {
+	$routes->get('artikel', 'Artikel::admin_index');
+	$routes->add('artikel/add', 'Artikel::add');
+	$routes->add('artikel/edit/(:any)', 'Artikel::edit/$1');
+	$routes->get('artikel/delete/(:any)', 'Artikel::delete/$1');
+	});
+	
 
 /*
  * --------------------------------------------------------------------
