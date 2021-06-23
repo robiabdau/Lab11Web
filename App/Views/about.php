c41248a7880fb126e9507656c2f8c102888f48d8
Robi Abda'u <81896427+robiabdau@users.noreply.github.com>
2021-06-23 21:58:21 +0700

    Add files via upload

--- App/Views/about.php
+++ App/Views/about.php
@@ -1,5 +1,20 @@
-<?= $this->include('template/header'); ?>
-<h1><?= $title; ?></h1>
-<hr>
-<p><?= $content; ?></p>
-<?= $this->include('template/footer'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title; ?></title>
+</head>
+<body>
+    <?= $this->include('template/header'); ?>
+    <section id="introduce">
+        <div class="row">
+            <img src="Riska.jpg" title="Riska Puspa Anggraeni Putri" alt="Riska Puspa Anggraeni Putri" class="image-circle" width="230"
+            style="float: left; border: 2px solid black;">
+            <h1>Hello!</h1>
+            <p>Nama saya Riska Puspa Anggraeni Putri. Saya adalah seorang mahasiswa dari <i>Universitas Pelita Bangsa</i> yang saat ini sedang
+                mempelajari materi PHP Framework (Codeigniter) dalam mata kuliah <i>Pemrograman Web</i>.</p>
+        </div>
+    </section>
+    <?= $this->include('template/footer'); ?>
+</body>
+</html>
